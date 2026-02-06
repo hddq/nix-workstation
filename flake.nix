f99b01b00756230dc887179d5ac711cbc99f3656
hddq <125512521+hddq@users.noreply.github.com>
2026-02-06 21:19:52 +0100

    feat: add helium browser, set default browser

--- flake.nix
+++ flake.nix
@@ -8,6 +8,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    helium.url = "github:FKouhai/helium2nix/main";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
--- home.nix
+++ home.nix
@@ -5,12 +5,24 @@
   home.homeDirectory = "/home/hddq";
 
   home.packages = with pkgs; [
-    inputs.zen-browser.packages."${pkgs.system}".default
+    inputs.zen-browser.packages."${pkgs.stdenv.hostPlatform.system}".default
+    inputs.helium.defaultPackage."${pkgs.stdenv.hostPlatform.system}"
     feishin
     obsidian
     fastfetch
     vscode
   ];
+
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "zen-beta.desktop";
+      "x-scheme-handler/http" = "zen-beta.desktop";
+      "x-scheme-handler/https" = "zen-beta.desktop";
+      "x-scheme-handler/about" = "zen-beta.desktop";
+      "x-scheme-handler/unknown" = "zen-beta.desktop";
+    };
+  };
 
   programs.git = {
     enable = true;
