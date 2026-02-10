600e3860fd56ef4e54a4bda52b1251c84b676065
hddq <125512521+hddq@users.noreply.github.com>
2026-02-10 02:15:19 +0100

    feat: add custom keybindings for terminal in Gnome settings

--- home/hddq/features/desktop.nix
+++ home/hddq/features/desktop.nix
@@ -5,6 +5,16 @@
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
     };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Super>q";
+      command = "kgx";
+      name = "Open Terminal";
+    };
   };
 
   gtk = {
