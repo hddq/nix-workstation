d7f10e94fc751bc835abe5672c2afea3ffc02464
hddq <125512521+hddq@users.noreply.github.com>
2026-02-10 02:19:27 +0100

    feat: add workspace keybindings configuration for Gnome

--- home/hddq/features/desktop.nix
+++ home/hddq/features/desktop.nix
@@ -6,6 +6,38 @@
       color-scheme = "prefer-dark";
     };
 
+    "org/gnome/mutter" = {
+      dynamic-workspaces = false;
+    };
+
+    "org/gnome/desktop/wm/preferences" = {
+      num-workspaces = 10;
+    };
+
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      switch-to-workspace-5 = [ "<Super>5" ];
+      switch-to-workspace-6 = [ "<Super>6" ];
+      switch-to-workspace-7 = [ "<Super>F1" ];
+      switch-to-workspace-8 = [ "<Super>F2" ];
+      switch-to-workspace-9 = [ "<Super>F3" ];
+      switch-to-workspace-10 = [ "<Super>F4" ];
+
+      move-to-workspace-1 = [ "<Super><Shift>1" ];
+      move-to-workspace-2 = [ "<Super><Shift>2" ];
+      move-to-workspace-3 = [ "<Super><Shift>3" ];
+      move-to-workspace-4 = [ "<Super><Shift>4" ];
+      move-to-workspace-5 = [ "<Super><Shift>5" ];
+      move-to-workspace-6 = [ "<Super><Shift>6" ];
+      move-to-workspace-7 = [ "<Super><Shift>F1" ];
+      move-to-workspace-8 = [ "<Super><Shift>F2" ];
+      move-to-workspace-9 = [ "<Super><Shift>F3" ];
+      move-to-workspace-10 = [ "<Super><Shift>F4" ];
+    };
+
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
     };
