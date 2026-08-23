9e582e5eab0f71afb0cefc61e93a38b52901e44a
hddq <git@hddq.org>
2026-08-23 23:36:55 +0200

    fix(vpn): update PIA root CA cert to SHA256 version and add gawk

--- modules/system/pia-vpn.nix
+++ modules/system/pia-vpn.nix
@@ -35,13 +35,20 @@
     yWJftjpdRM0+925Y6Cl08SWhge6kgeswgegxCzAJBgNVBAYTAlVTMQswCQYDVQQI
     EwJDQTETMBEGA1UEBxMKTG9zQW5nZWxlczEgMB4GA1UEChMXUHJpdmF0ZSBJbnRl
     cm5ldCBBY2Nlc3MxIDAeBgNVBAsTF1ByaXZhdGUgSW50ZXJuZXQgQWNjZXNzMSAw
-    HgYDVQQDExdQcml2YXRlIEludGVybmV0IEFjY2VzczEgMB4GA1UEKRMdc2VjdXJl
-    QHByaXZhdGVpbnRlcm5ldGFjY2Vzcy5jb20xLzAtBgkqhkiG9w0BCQEWIHNlY3Vy
-    ZUBwcml2YXRlaW50ZXJuZXRhY2Nlc3MuY29tggkAnS7684Nkme0wDAYDVR0TBAUw
-    AwEB/zANBgkqhkiG9w0BAQUFAAOCAgEAjF7nN3qQdK8Z3Htz6k8E7T177/XkFk8m
-    g19mIb7Sffx6QJ4qY6Qj8rYyQ0/xX7aFp0k6/yN8xY8Z3s9g6R/38ZJd/F8e5P2k
-    G0k7q69W4Ym/9Hw1aJ2R8sR5oK9Q8K/9Xz7g3lY0b4sT8Vw/7hV0G6m7VzQv7M5
-    M7aH9l2Q4j5g8Uf9qU2Q2lqT9yM/xW1qfT8o5y4a5k8Q==
+    HgYDVQQDExdQcml2YXRlIEludGVybmV0IEFjY2VzczEgMB4GA1UEKRMXUHJpdmF0
+    ZSBJbnRlcm5ldCBBY2Nlc3MxLzAtBgkqhkiG9w0BCQEWIHNlY3VyZUBwcml2YXRl
+    aW50ZXJuZXRhY2Nlc3MuY29tggkAnS7684Nkme0wDAYDVR0TBAUwAwEB/zANBgkq
+    hkiG9w0BAQ0FAAOCAgEAJsfhsPk3r8kLXLxY+v+vHzbr4ufNtqnL9/1Uuf8NrsCt
+    pXAoyZ0YqfbkWx3NHTZ7OE9ZRhdMP/RqHQE1p4N4Sa1nZKhTKasV6KhHDqSCt/dv
+    Em89xWm2MVA7nyzQxVlHa9AkcBaemcXEiyT19XdpiXOP4Vhs+J1R5m8zQOxZlV1G
+    tF9vsXmJqWZpOVPmZ8f35BCsYPvv4yMewnrtAC8PFEK/bOPeYcKN50bol22QYaZu
+    LfpkHfNiFTnfMh8sl/ablPyNY7DUNiP5DRcMdIwmfGQxR5WEQoHL3yPJ42LkB5zs
+    6jIm26DGNXfwura/mi105+ENH1CaROtRYwkiHb08U6qLXXJz80mWJkT90nr8Asj3
+    5xN2cUppg74nG3YVav/38P48T56hG1NHbYF5uOCske19F6wi9maUoto/3vEr0rnX
+    JUp2KODmKdvBI7co245lHBABWikk8VfejQSlCtDBXn644ZMtAdoxKNfR2WTFVEwJ
+    iyd1Fzx0yujuiXDROLhISLQDRjVVAvawrAtLZWYK31bY7KlezPlQnl/D9Asxe85l
+    8jO5+0LdJ6VyOs/Hd4w52alDW/MFySDZSfQHMTIc30hLBJ8OnCEIvluVQQ2UQvoW
+    +no177N9L2Y+M9TcTA62ZyMXShHQGeh20rb4kK8f+iFX8NxtdHVSkxMEFSfDDyQ=
     -----END CERTIFICATE-----
   '';
 
@@ -65,6 +72,7 @@
       wireguard-tools
       iproute2
       iptables
+      gawk
       coreutils
       gnugrep
       findutils
@@ -136,7 +144,7 @@
       PUBKEY=$(echo "$PRIVKEY" | wg pubkey)
 
       echo "Registering WireGuard public key with PIA server..."
-      REGISTER_RESP=$(curl -s -f -G --max-time 15 \
+      REGISTER_RESP=$(curl -s -G --max-time 15 \
         --connect-to "$SERVER_CN::$SERVER_IP:" \
         --cacert "${piaCaCert}" \
         "https://$SERVER_CN:1337/addKey" \
