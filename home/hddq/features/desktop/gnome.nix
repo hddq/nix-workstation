efa91d30455df143a364328e2eb29b0e2b36d8ef
hddq <git@hddq.org>
2026-09-25 09:26:28 +0200

    feat(desktop): switch workspace keybinds to 1-10

--- home/hddq/features/desktop/gnome.nix
+++ home/hddq/features/desktop/gnome.nix
@@ -45,10 +45,10 @@
         switch-to-workspace-4 = ["<Super>4"];
         switch-to-workspace-5 = ["<Super>5"];
         switch-to-workspace-6 = ["<Super>6"];
-        switch-to-workspace-7 = ["<Super>F1"];
-        switch-to-workspace-8 = ["<Super>F2"];
-        switch-to-workspace-9 = ["<Super>F3"];
-        switch-to-workspace-10 = ["<Super>F4"];
+        switch-to-workspace-7 = ["<Super>7"];
+        switch-to-workspace-8 = ["<Super>8"];
+        switch-to-workspace-9 = ["<Super>9"];
+        switch-to-workspace-10 = ["<Super>0"];
 
         move-to-workspace-1 = ["<Super><Shift>1"];
         move-to-workspace-2 = ["<Super><Shift>2"];
@@ -56,10 +56,10 @@
         move-to-workspace-4 = ["<Super><Shift>4"];
         move-to-workspace-5 = ["<Super><Shift>5"];
         move-to-workspace-6 = ["<Super><Shift>6"];
-        move-to-workspace-7 = ["<Super><Shift>F1"];
-        move-to-workspace-8 = ["<Super><Shift>F2"];
-        move-to-workspace-9 = ["<Super><Shift>F3"];
-        move-to-workspace-10 = ["<Super><Shift>F4"];
+        move-to-workspace-7 = ["<Super><Shift>7"];
+        move-to-workspace-8 = ["<Super><Shift>8"];
+        move-to-workspace-9 = ["<Super><Shift>9"];
+        move-to-workspace-10 = ["<Super><Shift>0"];
       };
 
       "org/gnome/settings-daemon/plugins/media-keys" = {
--- home/hddq/features/desktop/hyprland/binds.nix
+++ home/hddq/features/desktop/hyprland/binds.nix
@@ -20,29 +20,29 @@
         "$mainMod, up, movefocus, u"
         "$mainMod, down, movefocus, d"
 
-        # Switch workspaces with mainMod + [1-6, F1-F4]
+        # Switch workspaces with mainMod + [0-9]
         "$mainMod, 1, workspace, 1"
         "$mainMod, 2, workspace, 2"
         "$mainMod, 3, workspace, 3"
         "$mainMod, 4, workspace, 4"
         "$mainMod, 5, workspace, 5"
         "$mainMod, 6, workspace, 6"
-        "$mainMod, F1, workspace, 7"
-        "$mainMod, F2, workspace, 8"
-        "$mainMod, F3, workspace, 9"
-        "$mainMod, F4, workspace, 10"
+        "$mainMod, 7, workspace, 7"
+        "$mainMod, 8, workspace, 8"
+        "$mainMod, 9, workspace, 9"
+        "$mainMod, 0, workspace, 10"
 
-        # Move active window to a workspace with mainMod + SHIFT + [1-6, F1-F4]
+        # Move active window to a workspace with mainMod + SHIFT + [0-9]
         "$mainMod SHIFT, 1, movetoworkspace, 1"
         "$mainMod SHIFT, 2, movetoworkspace, 2"
         "$mainMod SHIFT, 3, movetoworkspace, 3"
         "$mainMod SHIFT, 4, movetoworkspace, 4"
         "$mainMod SHIFT, 5, movetoworkspace, 5"
         "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, F1, movetoworkspace, 7"
-        "$mainMod SHIFT, F2, movetoworkspace, 8"
-        "$mainMod SHIFT, F3, movetoworkspace, 9"
-        "$mainMod SHIFT, F4, movetoworkspace, 10"
+        "$mainMod SHIFT, 7, movetoworkspace, 7"
+        "$mainMod SHIFT, 8, movetoworkspace, 8"
+        "$mainMod SHIFT, 9, movetoworkspace, 9"
+        "$mainMod SHIFT, 0, movetoworkspace, 10"
 
         # Screenshots
         "$mainMod SHIFT, S, exec, hyprshot -m region -o ~/Pictures/Screenshots --freeze"
